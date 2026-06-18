b8aed2df041228cd68ff3c2915b5edb424f09953
Chase Owens <chasejonathanowens@gmail.com>
2026-06-18 01:56:07 -0500

    change loader

--- clip-web-prod/src/routes/+page.ts
+++ clip-web-prod/src/routes/+page.ts
@@ -1,24 +1,13 @@
 import type { PageLoad } from "./$types";
 import type {RootContent } from "../../../shared/types/RootContent"
-import rootContentData from '$lib/data/root-content.json'
-import { error } from "@sveltejs/kit";
 
-const fallbackContent = rootContentData as unknown as RootContent
+export const load:PageLoad = async ({ fetch }) => {
+	const response = await fetch('/data/root-content.json');
 
-export const load: PageLoad = async ({fetch}) => {
-  if (import.meta.env.VITE_IS_MOCK === 'true') {
-    return fallbackContent
-  }
-
-  try {
-		const res = await fetch('/data/root-content.json');
-
-		if (!res.ok) {
-			throw error(500, 'Content failed to load');
-		}
-
-		return (await res.json()) as RootContent;
-	} catch (err) {
-		throw error(500, `Content failed to load ${err}`);
+	if (!response.ok) {
+		throw new Error('Failed to load root content');
 	}
-}
+
+	return (await response.json()) as RootContent;
+
+};
